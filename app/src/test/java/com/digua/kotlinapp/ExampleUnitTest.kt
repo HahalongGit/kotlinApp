a09873847493402e443a53c0ac8e8ca331af9341
LonglongLiu <longlong.liu@android.com>
2021-03-16 16:21:33 +0800

    添加协程的基本语法1

--- app/src/test/java/com/digua/kotlinapp/ExampleUnitTest.kt
+++ app/src/test/java/com/digua/kotlinapp/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package com.digua.kotlinapp
 
 import com.digua.kotlinapp.login.bean.LoginParam
+import kotlinx.coroutines.*
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,6 +12,11 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    companion object{
+        val TAG = ExampleUnitTest.javaClass.name+"---"
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
@@ -22,5 +28,28 @@ class ExampleUnitTest {
         print("Log输出")
     }
 
+    @Test
+    fun testCorountines(){
+        GlobalScope.launch {
+            println("start------------")
+            for ( i in 1..3){
+                delay(330)
+                println("i = $i --> "+ System.currentTimeMillis())
+            }
+            println(TAG+"--thread-1--"+Thread.currentThread().name)
+            println("world ")
+            test()
+        }
+        println("hello")
+        Thread.sleep(3000)  //// 阻塞主线程 3 秒钟来保证 JVM 存活 是什么意思？ 注释掉就launch就不执行了
+        println(TAG+"--thread-2--"+Thread.currentThread().name);
+    }
+
+    private suspend fun test(){
+        println(TAG+"thread-test-1"+Thread.currentThread().name);
+        withContext(Dispatchers.IO){
+            println(TAG+"thread-test-2"+Thread.currentThread().name);
+        }
+    }
 
 }
