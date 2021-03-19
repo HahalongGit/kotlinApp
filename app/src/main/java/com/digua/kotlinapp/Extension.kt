d3bced1eb6a0405acfa7c4474b2408fdfd2893fe
LonglongLiu <longlong.liu@android.com>
2021-03-19 18:01:58 +0800

    扩展函数定义

---
+++ app/src/main/java/com/digua/kotlinapp/Extension.kt
@@ -0,0 +1,13 @@
+package com.digua.kotlinapp
+
+fun List<String>.getLongestString():String{
+    println("getLongestString")
+    return "这是longest的返回值"
+}
+/**
+ * 扩展函数定义的类
+ *
+ * @author RunningDigua
+ * @date 2021/3/19
+ */
+class Extension
